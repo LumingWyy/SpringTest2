a3b01574e02fff77f7619b1caee12b85cc1e2706
wlm <599290100@qq.com>
2022-01-21 22:54:51 +0900

    scan

--- src/main/java/com/test/bean/Student.java
+++ src/main/java/com/test/bean/Student.java
@@ -1,16 +1,27 @@
 package com.test.bean;
 
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 @ToString
 @Component
 public class Student {
     int age;
     String name;
+
     Card card;
 
     public void setName(String name) {
         this.name = name;
+    }
+    @Autowired
+    @Qualifier("bbb")
+    public void setCard(Card card) {
+        System.out.println("set() inject prpty");
+        this.card = card;
     }
 }
--- src/main/java/com/test/config/MainConfiguration.java
+++ src/main/java/com/test/config/MainConfiguration.java
@@ -2,6 +2,7 @@ package com.test.config;
 
 import com.test.bean.Card;
 import com.test.bean.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 
 @ComponentScan("com.test.bean")
@@ -10,17 +11,17 @@ import org.springframework.context.annotation.*;
 //})
 @Configuration
 public class MainConfiguration {
-//
+
+
+    @Bean("aaaaa")
+    public Card card() {
+        return new Card();
+    }
+
 //    @Bean
-//    @Scope("prototype")
-//    public Card card() {
-//        return new Card();
-//    }
-//
-//    @Bean
-//    public Student student(){
+//    public Student student(@Autowired Card card){
 //        Student student= new Student();
-//        student.setName("Tom");
+//        student.setCard(card);
 //        return student;
 //    }
 }
